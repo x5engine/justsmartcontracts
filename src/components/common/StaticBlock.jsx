fbf6351bd694191cdcca09bc14039c543ceb9338
X5Engine <ucefkh@01tek.com>
2020-04-14 02:27:31 +0100

    about changes

--- src/components/common/StaticBlock.jsx
+++ src/components/common/StaticBlock.jsx
@@ -9,14 +9,12 @@ import ReactMarkdown from 'react-markdown';
 class StaticBlock extends React.Component {
     constructor(props) {
         super(props);
-
-        //info - markdown or text to be rendered
-        this.state = {info: ''};
+        this.state = {info: "# What is it?\r\nX5 Smart Contract Viewer is a place where you can “bookmark” certain Ethereum smart contracts and then watch for their properties and events or call functions in a user-friendly manner.\r\n\r\n\r\nX5 Smart Contract Viewer was designed for Ethereum developers in the first place. But anybody can make use of it.\r\n\r\n\r\nX5 Smart Contract Viewer is a front-end tool. All the data including your contracts' addresses and their ABI is stored in the web browser's local storage.\r\n\r\n\r\nX5 Smart Contract Viewer is an open-source project. Our repository is [here](https://github.com/x5engine/justsmartcontracts), feel free to study it and join us."}
         this.sendQuery = this.sendQuery.bind(this);
     }
 
     componentDidMount() {
-        this.sendQuery();
+        // this.sendQuery();
     }
 
     sendQuery() {
